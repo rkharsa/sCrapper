e4a96a6e7291a6ba79ce0493ed44c27f933890e3
Frantz <ediscream@gmail.com>
2019-11-17 20:32:24 +0100

    clean

--- taskManager.c
+++ taskManager.c
@@ -6,17 +6,27 @@
 #include "manager.h"
 #include "file.h"
 
+/**
+ *
+ * @return
+ * return the current timestamp
+ */
 long getCurrentTime() {
     time_t current_time;
     current_time = time(NULL);
     long t = current_time;
     return t;
 }
-
-int executeIsAvailable(int val) {
-    return val;
-}
-
+/**
+ *
+ * @param currentTime
+ * @param hours
+ * @param minutes
+ * @param seconds
+ * @return
+ *
+ * return a timestamp incremented
+ */
 long incrementTime(long currentTime, int hours, int minutes, int seconds) {
     long destime = currentTime;
     long h = hours * 3600;
@@ -25,21 +35,26 @@ long incrementTime(long currentTime, int hours, int minutes, int seconds) {
     return destime;
 
 }
-
-void writeInFile(char* taskname, char* actionName) {
+/**
+ *
+ * @param taskname
+ * @param actionName
+ *
+ * for versionning write in a file when an action is executed
+ */
+void writeInFile(char *taskname, char *actionName) {
     time_t now;
     time(&now);
-    char* time = ctime(&now);
+    char *time = ctime(&now);
     char message[100];
     strcpy(message, "Nom de l'action: ");
     strcat(message, actionName);
     strcat(message, " Nom de la tâche: ");
     strcat(message, taskname);
     strcat(message, " Date: ");
-    char* fn = malloc(sizeof(char) * 100);
+    char *fn = malloc(sizeof(char) * 100);
     sprintf(fn, "%s.txt", actionName);
-    printf("%s\n", fn);
-    FILE* f = fopen(fn, "a+");
+    FILE *f = fopen(fn, "a+");
     if (f != NULL) {
         fputs(message, f);
         fputs(time, f);
@@ -48,7 +63,15 @@ void writeInFile(char* taskname, char* actionName) {
     free(fn);
 }
 
-char* findValueByKey(char* key, Action action) {
+/**
+ *
+ * @param key
+ * @param action
+ * @return
+ *
+ * find the value of a key for an action
+ */
+char *findValueByKey(char *key, Action action) {
     for (int j = 0; j < action.optionsLength; j++) {
         if (strcmp(action.keys[j], key) == 0) {
             return action.values[j];
@@ -57,50 +80,62 @@ char* findValueByKey(char* key, Action action) {
     return NULL;
 }
 
-int findIntValueByKey(Action action, char* key) {
+/**
+ *
+ * @param action
+ * @param key
+ * @return
+ * find the value  integer of a key for an action
+ */
+int findIntValueByKey(Action action, char *key) {
 
     for (int i = 0; i < action.optionsLength; ++i) {
-        if(strcmp(action.keys[i], key) == 0) {
+        if (strcmp(action.keys[i], key) == 0) {
             return atoi(action.values[i]);
         }
     }
 
     return 0;
 }
-
+/**
+ *
+ * @param arg
+ * @return
+ * thread for executeAction
+ */
 void *taskthread(void *arg) {
-    Action *a = (struct Action *) arg;
-//    executeAction(*a);
-    //free(a);
+    Task *t = (struct Task *) arg;
+    for (int i = 0; i < 1; ++i) {
+        for (int j = 0; j < t[i].actionsLength; ++j) {
+            executeAction(t[i], t[i].actions[j]);
+        }
+    }
     return NULL;
 }
 
 void executeTags(Action action, char *tags, int maxDepth) {
     int tagsLength = 0;
-    char** tagsList = strToArrayStr(tags + 1, &tagsLength, ",)");
+    char **tagsList = strToArrayStr(tags + 1, &tagsLength, ",)");
 
     removeDirectory(action.name);
     createDirectoryTreeStruct(action.name);
 
-    if(maxDepth <= 0) {
-        execute(tagsList, action.url, tagsLength, action.name,"0");
+    if (maxDepth <= 0) {
+        execute(tagsList, action.url, tagsLength, action.name, "0");
         return;
     }
 
     firstWave(action.url);
-    if(maxDepth > 1) {
+    if (maxDepth > 1) {
         for (int i = 0; i < maxDepth; ++i) {
             nextWave(i);//faire un sort puis uniq puis merge puis resort et uniq <3
         }
     }
-    printf("EXECUTE TAG\n");
-
-    execute(tagsList, action.url, tagsLength, action.name,"0");
+    execute(tagsList, action.url, tagsLength, action.name, "0");
 }
 
-void executeAction(Task task, Action action) { // parcourir chaque option
-    printf("EXECUTE ACTION\n");
-    char* value = findValueByKey("versionning", action);
+void executeAction(Task task, Action action) {
+    char *value = findValueByKey("versionning", action);
     if (strcmp(value, "on") == 0) {
         writeInFile(task.name, action.name);
     }
@@ -113,15 +148,19 @@ void executeAction(Task task, Action action) { // parcourir chaque option
         } else if (strcmp(action.keys[i], "tags") == 0) {
 
             executeTags(action, action.values[i], findIntValueByKey(action,
-                    "max-depth"));
+                                                                    "max-depth"));
         }
     }
 }
-
-void taskExec(Task* task, int taskLenght) {
+/**
+ *
+ * @param task
+ * @param taskLenght
+ * check when a task need to be executed create a thread
+ */
+void taskExec(Task *task, int taskLenght) {
     long fromtime = getCurrentTime();
     while (1) {
-        printf("WHGILE\n");
         for (int i = 0; i < taskLenght; i++) {
             long newTime = getCurrentTime();
             long destime = 0;
@@ -134,13 +173,12 @@ void taskExec(Task* task, int taskLenght) {
             destime = task[i].nextOccurence;
             if (destime == newTime) {
                 for (int j = 0; j < task[i].actionsLength; j++) {
-                    printf("FOR\n");
-                    char* value = findValueByKey("versionning", task[i].actions[j]);
+                    char *value = findValueByKey("versionning", task[i].actions[j]);
                     if (strcmp(value, "on") == 0) {
                         writeInFile(task[i].name, task[i].actions[j].name);
                     }
-                    printf("NEW ACTION\n");
-                    executeAction(task[i], task[i].actions[j]);
+                    pthread_t thread1;
+                    pthread_create(&thread1, NULL, taskthread, &task[i]);
                 }
                 task[i].nextOccurence = incrementTime(newTime, hours, minutes, seconds);
             }
